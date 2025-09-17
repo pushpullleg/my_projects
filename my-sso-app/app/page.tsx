35f795316254e9b36a050d01d92554c47f891808
pushpullleg <mravichandr@leomail.tamuc.edu>
2025-09-17 18:55:13 -0500

    update on the main page, thanking tejaswini

--- my-sso-app/app/page.tsx
+++ my-sso-app/app/page.tsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <main>
       <h1>Hello, Next.js!</h1>
-      <p>This is my first edit 🚀</p>
+      <p> 🚀</p>
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+        <h1 style={{ fontSize: "3rem", fontWeight: "bold" }}>Thanks for rearranging things, my love !</h1>
+      </div>
     </main>
   )
 }
