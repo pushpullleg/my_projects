e40a255b6dd665b37c4a4a27a6bcebd50163973b
pushpullleg <mravichandr@leomail.tamuc.edu>
2025-09-21 20:37:43 -0500

    removed signin and modified page.tsx

--- my-sso-app/app/page.tsx
+++ my-sso-app/app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
 
-import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center min-h-screen flex-col gap-6">
       <SignedOut>
         <h1 className="text-2xl font-bold">Welcome! Please sign in 👇</h1>
-        <SignIn path="/sign-in"  signUpUrl="/sign-up" />
       </SignedOut>
 
       <SignedIn>
