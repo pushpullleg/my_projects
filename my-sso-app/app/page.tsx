90fc2ee8a1d117f1f17886b0f0a569da7a0110cc
pushpullleg <mravichandr@leomail.tamuc.edu>
2025-09-21 20:34:20 -0500

    removed page tsx from sign in and removed routing='path'

--- my-sso-app/app/page.tsx
+++ my-sso-app/app/page.tsx
@@ -4,16 +4,14 @@ import { SignedIn, SignedOut, SignIn, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen gap-6">
+    <main className="flex justify-center items-center min-h-screen flex-col gap-6">
       <SignedOut>
         <h1 className="text-2xl font-bold">Welcome! Please sign in 👇</h1>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <SignIn path="/sign-in"  signUpUrl="/sign-up" />
       </SignedOut>
 
       <SignedIn>
-        <h1 className="text-3xl font-bold text-center">
-          Thanks for rearranging things, my love! 💙
-        </h1>
+        <h1 className="text-2xl font-bold">🎉 You are logged in!</h1>
         <UserButton afterSignOutUrl="/" />
       </SignedIn>
     </main>
