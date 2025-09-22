61d6e17b2a0e35f2a542efbb4dd0e94e4c71f176
pushpullleg <mravichandr@leomail.tamuc.edu>
2025-09-21 20:45:20 -0500

    adding a sign in button , connecting home page to sign in page

--- my-sso-app/app/page.tsx
+++ my-sso-app/app/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex justify-center items-center min-h-screen flex-col gap-6">
       <SignedOut>
         <h1 className="text-2xl font-bold">Welcome! Please sign in 👇</h1>
+        <Link
+          href="/sign_in"
+          className="mt-4 px-6 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+        >
+          Sign In
+        </Link>
       </SignedOut>
 
       <SignedIn>
